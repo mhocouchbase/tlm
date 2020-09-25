94aa51c2356478428d59ecee91a3aef85247c045
Ming Ho <ming.ho@couchbase.com>
2020-09-24 17:15:25 -0700

    CBD-3674, update v8 to 8.5 -Ming Ho

--- deps/packages/v8/v8_windows.bat
+++ deps/packages/v8/v8_windows.bat
@@ -14,15 +14,16 @@ set DEPOT_TOOLS_WIN_TOOLCHAIN=0
 
 rem Set up gclient config for tag to pull for v8, then do sync
 rem (this handles the 'fetch v8' done by the usual process)
-echo solutions = [ { "url": "https://github.com/couchbasedeps/v8-mirror.git@7.1.321","managed": False, "name": "v8", "deps_file": "DEPS", }, ]; > .gclient
+echo solutions = [ { "url": "https://github.com/couchbasedeps/v8-mirror.git@8.5.210.26","managed": False, "name": "v8", "deps_file": "DEPS", }, ]; > .gclient
 call gclient sync || goto error
 echo on
 
 rem Actual v8 configure and build steps - we build debug and release.
 cd v8
 rem Use double-quoting here because .bat quote removal is inscrutable.
-set V8_ARGS=target_cpu=""x64"" is_component_build=true v8_enable_backtrace=true v8_use_snapshot=true v8_use_external_startup_data=false
+set V8_ARGS=target_cpu=""x64"" use_custom_libcxx=true is_component_build=true v8_enable_backtrace=true v8_use_snapshot=true v8_use_external_startup_data=false v8_enable_pointer_compression=false
 call gn gen out.gn/x64.release --args="%V8_ARGS% is_debug=false" || goto error
+
 echo on
 call ninja -C out.gn/x64.release || goto error
 echo on
@@ -35,24 +36,33 @@ rem Copy right stuff to output directory.
 mkdir %INSTALL_DIR%\lib\Release
 mkdir %INSTALL_DIR%\lib\Debug
 mkdir %INSTALL_DIR%\include\libplatform
+mkdir %INSTALL_DIR%\include\cppgc
 mkdir %INSTALL_DIR%\include\unicode
 
 cd out.gn\x64.release
 copy v8.dll* %INSTALL_DIR%\lib\Release || goto error
 copy v8_lib* %INSTALL_DIR%\lib\Release || goto error
+copy cppgc.* %INSTALL_DIR%\lib\Release || goto error
 copy icu*.* %INSTALL_DIR%\lib\Release || goto error
+copy libc*.* %INSTALL_DIR%\lib\Release || goto error
+copy zlib*.* %INSTALL_DIR%\lib\Release || goto error
 del %INSTALL_DIR%\lib\Release\*.ilk || goto error
 
 cd ..\..\out.gn\x64.debug
 copy v8.dll* %INSTALL_DIR%\lib\Debug || goto error
 copy v8_lib* %INSTALL_DIR%\lib\Debug || goto error
+copy cppgc.* %INSTALL_DIR%\lib\Debug || goto error
 copy icu*.* %INSTALL_DIR%\lib\Debug || goto error
+copy libc*.* %INSTALL_DIR%\lib\Debug || goto error
+copy zlib*.* %INSTALL_DIR%\lib\Debug || goto error
 del %INSTALL_DIR%\lib\Debug\*.ilk || goto error
 
 cd ..\..\include
 copy v8*.h %INSTALL_DIR%\include || goto error
 cd libplatform
 copy *.h %INSTALL_DIR%\include\libplatform || goto error
+cd ..\cppgc
+copy *.h %INSTALL_DIR%\include\cppgc || goto error
 
 cd ..\..\third_party\icu\source\common\unicode
 copy *.h %INSTALL_DIR%\include\unicode || goto error
